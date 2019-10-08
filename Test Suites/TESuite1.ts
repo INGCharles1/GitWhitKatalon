bea2f21a8ffbf61956641d743b4a8eea87b3a20e
AS468AE <AS468AE@GT2411990W1.ey.net>
2019-10-08 15:51:08 -0600

    Version 1.4 esta es una prueba alv

---
+++ Test Suites/TESuite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TESuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>560c2694-d807-46d6-af56-ef6d187f81b2</testSuiteGuid>
+   <testCaseLink>
+      <guid>fe720205-d7ff-442a-8d61-0003dba4e8fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
